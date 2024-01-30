46dcc166801d61ed7cac058e335090606697303d
Szymon Kilian <szymonkilian98@o2.pl>
2024-01-30 14:20:00 +0100

    set up helper to send data

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
+
 if ( !function_exists('spy') ) {
     function spy($object)
     {
+        $data = ['data' => $object];
+
+        try {
+            // Send a POST request to the specified endpoint
+            Http::post('http://localhost:8100/api/spies/', $data);
+        } catch (\Exception $e) {
+            // Handle exceptions
+            // Consider logging this error
+            echo 'Request failed: ' . $e->getMessage();
+        }
+
         return $object;
     }
 }
+
+
