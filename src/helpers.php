ce1f99989673ce3c9d18cd8d9da56698609d7c2e
Szymon Kilian <szymonkilian98@o2.pl>
2024-01-30 16:50:44 +0100

    Dodanie nazwy pliku i lini do requesta

--- src/helpers.php
+++ src/helpers.php
@@ -3,28 +3,39 @@
 use Illuminate\Support\Facades\Http;
 
 if ( !function_exists('spyOn') ) {
-function spyOn($object)
-{
-    $data = ['data' => $object];
+    function spyOn($object)
+    {
+        $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
 
-    try {
-        // Send a POST request to the specified endpoint
-        $response = Http::post('localhost:8100/api/spies/', $data);
+        $file = basename($backtrace[0]['file']);
+        $line = $backtrace[0]['line'];
 
-        // Optionally, check the response status code or content here
-        if ($response->successful()) {
-            // Return the original object or possibly the response itself
-            return $response->json();
+        $data = [
+            'data' => $object,
+            'file' => $file,
+            'line' => $line,
+        ];
+
+        try {
+            // Send a POST request to the specified endpoint
+            $response = Http::post('localhost:8100/api/spies/', $data);
+
+            // Optionally, check the response status code or content here
+            if ($response->successful()) {
+                // Return the original object or possibly the response itself
+                return $response->json();
+            }else{
+                return $response;
+            }
+        } catch (\Exception $e) {
+            // Handle exceptions and return a detailed error message
+            return response()->json([
+                'error' => 'Request failed',
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 500);
         }
-    } catch (\Exception $e) {
-        // Handle exceptions and return a detailed error message
-        return response()->json([
-            'error' => 'Request failed',
-            'message' => $e->getMessage(),
-            'code' => $e->getCode()
-        ], 500);
     }
-}
 }
 
 
